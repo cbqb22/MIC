3be2d46dd53af7c19bb7d0fb9b81ea9c4fe80d0c
cbqb22 <shu_masafumi@yahoo.co.jp>
2016-10-24 01:11:38 +0900

    プロジェクトをコミット

---
+++ MIC/MIC.Wpf/Controls/Animations/FlowParticles/FlowParticles.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MIC.Wpf.Controls.Animations.FlowParticles
+{
+    /// <summary>
+    /// FlowParticles.xaml の相互作用ロジック
+    /// </summary>
+    public partial class FlowParticles : UserControl
+    {
+        public FlowParticles()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private List<Path> _pathList;
+
+        public List<Path> PathList
+        {
+            get
+            {
+                return _pathList;
+            }
+
+            set
+            {
+                _pathList = value;
+            }
+        }
+
+        private void Init()
+        {
+            Action a = () => 
+            { Path p = new Path();
+                p.Height = 2;
+                p.Width = 2;
+            };
+        }
+
+    }
+}
---
+++ MIC/MIC.Wpf/Controls/Animations/Indicator/Indicator.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace MIC.Wpf.Controls.Animations.Indicator
+{
+    /// <summary>
+    /// Indicator.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Indicator : UserControl
+    {
+        public Indicator()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty IndicationThemeColorProperty =   DependencyProperty.Register(
+                                                                                   "IndicationThemeColor", 
+                                                                                   typeof(Color), 
+                                                                                   typeof(Indicator), 
+                                                                                   new PropertyMetadata(Colors.Orange));
+
+        public Color IndicationThemeColor
+        {
+            get { return (Color)GetValue(IndicationThemeColorProperty); }
+            set { SetValue(IndicationThemeColorProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty DisplayTextProperty = DependencyProperty.Register(
+                                                                           "DisplayText",
+                                                                           typeof(string),
+                                                                           typeof(Indicator),
+                                                                           new PropertyMetadata());
+
+        public string DisplayText
+        {
+            get { return (string)GetValue(DisplayTextProperty); }
+            set { SetValue(DisplayTextProperty, value); }
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+
+}
